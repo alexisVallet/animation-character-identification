786940f6b6d3969d72524c073ad347897477925d
Yuki NAKAGAWA <ikasou@gmail.com>
2013-05-16 11:03:22 +0900

    First developing of ContourDetector.

---
+++ animation-charactor-identification for Mac/animation-charactor-identification for Mac/ContourDetectionforAnime.cpp
@@ -0,0 +1,9 @@
+//
+//  ContourDetectionforAnime.cpp
+//  animation-charactor-identification for Mac
+//
+//  Created by Yuki NAKAGAWA on 2013/05/07.
+//  Copyright (c) 2013年 Yuki & Alexis. All rights reserved.
+//
+
+#include "ContourDetectionforAnime.h"
---
+++ animation-charactor-identification for Mac/animation-charactor-identification for Mac/ContourDetectionforAnime.h
@@ -0,0 +1,14 @@
+//
+//  ContourDetectionforAnime.h
+//  animation-charactor-identification for Mac
+//
+//  Created by Yuki NAKAGAWA on 2013/05/07.
+//  Copyright (c) 2013年 Yuki & Alexis. All rights reserved.
+//
+
+#ifndef __animation_charactor_identification_for_Mac__ContourDetectionforAnime__
+#define __animation_charactor_identification_for_Mac__ContourDetectionforAnime__
+
+#include <iostream>
+
+#endif /* defined(__animation_charactor_identification_for_Mac__ContourDetectionforAnime__) */
